d30e4829e7bbf62573c523292083927a32e9e6be
kazu-taka <k.takahashi@nowall.co.jp>
2018-05-25 21:17:22 +0900

    登録機能作成

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,5 +3,18 @@ class NotesController < ApplicationController
     @note = Note.new
   end
   def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to notes_path
+    else
+      render :index
+    end
+  end
+  
+  private
+  
+  def note_params
+    # ストロングパラメータはフォームで入力した内容を受け取る
+    params.require(:note).permit(:title, :body, :user_id)
   end
 end
